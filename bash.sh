578bbcdcde5d673c2ccace26c74e4db657c4a779
Marek Pistorius <me@maniu.eu>
2023-01-24 17:27:16 +0100

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="mpist@users.noreply.github.com"
+CORRECT_NAME="mp"
+CORRECT_EMAIL="maniu@pm.me"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+
